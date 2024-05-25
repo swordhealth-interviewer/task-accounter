efefebaa3eb24b060f60296cc5ec3f75080a5d93
Uian Sol <usoldeveloper@gmail.com>
2024-05-25 20:41:14 -0300

    refactor: isolate constants and errors in its on file

--- internal/domain/usecases/task_create_usecase.go
+++ internal/domain/usecases/task_create_usecase.go
@@ -8,18 +8,6 @@ import (
 	"github.com/uiansol/task-accounter.git/internal/domain/entities"
 )
 
-type TaskCreateUseCaseError string
-
-const (
-	summaryMaxLength int = 2500
-
-	technicianRoleRequiredError TaskCreateUseCaseError = "only technicians can create tasks"
-	emptyTitleError             TaskCreateUseCaseError = "title is required"
-	emptySummaryError           TaskCreateUseCaseError = "summary is required"
-	summaryMaxLengthError       TaskCreateUseCaseError = "summary must have a maximum of 2500 characters"
-	saveError                   TaskCreateUseCaseError = "usecase error saving task"
-)
-
 type TaskCreateInput struct {
 	Title   string
 	Summary string

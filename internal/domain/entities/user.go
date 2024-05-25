960a8b6a20518990792cf56161ae6fba6eaf05ad
Uian Sol <usoldeveloper@gmail.com>
2024-05-25 10:56:41 -0300

    feat: add user entity with tests

---
+++ internal/domain/entities/user.go
@@ -0,0 +1,53 @@
+package entities
+
+import (
+	"errors"
+	"net/mail"
+
+	"github.com/google/uuid"
+)
+
+type Type string
+
+const (
+	Manager    Type = "manager"
+	Technician Type = "technician"
+)
+
+type User struct {
+	ID    string
+	Name  string
+	Email string
+	Role  Type
+}
+
+func NewUser(name string, address string, role string) (User, error) {
+	email, err := validMailAddress(address)
+	if err != nil {
+		return User{}, errors.New("invalid email address")
+	}
+
+	if role != "manager" && role != "technician" {
+		return User{}, errors.New("invalid role")
+	}
+
+	uuid := uuid.NewString()
+
+	user := User{
+		ID:    uuid,
+		Name:  name,
+		Email: email,
+		Role:  Type(role),
+	}
+
+	return user, nil
+}
+
+func validMailAddress(address string) (string, error) {
+	addr, err := mail.ParseAddress(address)
+	if err != nil {
+		return "", err
+	}
+
+	return addr.Address, nil
+}

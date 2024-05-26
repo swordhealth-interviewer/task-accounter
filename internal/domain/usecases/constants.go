d7b97ef2653fa194e949e085ea2d55f45cdbd761
Uian Sol <usoldeveloper@gmail.com>
2024-05-26 13:45:51 -0300

    feat: add task delete with tests

--- internal/domain/usecases/constants.go
+++ internal/domain/usecases/constants.go
@@ -4,11 +4,13 @@ type UseCaseError string
 
 const (
 	ErrorTechnicianRoleRequired UseCaseError = "only technicians can create tasks"
+	ErrorManagerRoleRequired    UseCaseError = "only managers can delete tasks"
 	ErrorTaskNotOwnedByUser     UseCaseError = "task is not owned by the user"
 	ErrorTaskClosed             UseCaseError = "closed tasks can not be updated"
 	ErrorInvalidTaskData        UseCaseError = "invalid task data"
 	ErrorCreateTask             UseCaseError = "error creating task"
 	ErrorSaveTask               UseCaseError = "error saving task on database"
+	ErrorDeleteTask             UseCaseError = "error deleting task on database"
 	ErrorFindTaskByID           UseCaseError = "error finding task on database"
 	ErrorFindAllTasks           UseCaseError = "error reading all tasks from database"
 	ErrorFindTasksByUser        UseCaseError = "error reading tasks by user from database"

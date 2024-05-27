59135f8c6e73537701628fc23be02d72c53478cd
Uian Sol <usoldeveloper@gmail.com>
2024-05-27 11:21:24 -0300

    feat: add read usecase with tests

---
+++ internal/domain/usecases/task_read_usecase.go
@@ -0,0 +1,48 @@
+package usecases
+
+import (
+	"errors"
+
+	"github.com/uiansol/task-accounter.git/internal/domain/adapters"
+	"github.com/uiansol/task-accounter.git/internal/domain/entities"
+)
+
+type TaskReadInput struct {
+	ID   string
+	User entities.User
+}
+
+type TaskReadOutput struct {
+	Task *entities.Task
+}
+
+type TaskReadUseCaseInterface interface {
+	Execute(input TaskReadInput) (TaskReadOutput, error)
+}
+
+type TaskReadUseCase struct {
+	TaskRepository adapters.TaskRepositoryInterface
+}
+
+func NewTaskReadUseCase(taskRepository adapters.TaskRepositoryInterface) TaskReadUseCase {
+	return TaskReadUseCase{
+		TaskRepository: taskRepository,
+	}
+}
+
+func (u TaskReadUseCase) Execute(input TaskReadInput) (TaskReadOutput, error) {
+	task, err := u.TaskRepository.FindByID(input.ID)
+	if err != nil {
+		return TaskReadOutput{}, err
+	}
+
+	if task.OwnerID != input.User.ID && input.User.Role != entities.UserRoleManager {
+		return TaskReadOutput{}, errors.New(string(ErrorTaskNotOwnedByUser))
+	}
+
+	output := TaskReadOutput{
+		Task: task,
+	}
+
+	return output, nil
+}
